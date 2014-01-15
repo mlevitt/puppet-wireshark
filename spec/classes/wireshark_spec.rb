969ef4b2dd5afb17fa5bae18d4fa781dee0d70bd
Michael Ferguson <mpherg@hotmail.com>
2014-01-14 17:07:41 -0700

    Move to Wireshard version 1.10.5

--- spec/classes/wireshark_spec.rb
+++ spec/classes/wireshark_spec.rb
@@ -3,7 +3,7 @@ describe 'wireshark' do
   it do
     should contain_package('Wireshark').with({
 			:provider => 'appdmg',
-			:source   => 'http://wiresharkdownloads.riverbed.com/wireshark/osx/Wireshark%201.8.6%20Intel%2064.dmg',
+			:source   => 'http://wiresharkdownloads.riverbed.com/wireshark/osx/Wireshark%201.10.5%20Intel%2064.dmg',
 		})
   end
 end
